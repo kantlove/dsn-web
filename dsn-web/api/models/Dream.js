159f31c3b13af672ccc889b5beed043def92cfcd
nminhtu94 <nminhtu94@gmail.com>
2014-10-28 22:27:50 +0700

    Add some models

---
+++ dsn-web/api/models/Dream.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+	attributes: {
+		id: {
+			type: 'int'
+		},
+
+		user_id: {
+			type: 'int'
+		},
+
+		text: {
+			type: 'string'
+		}
+	}
+}
---
+++ dsn-web/api/models/User.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+	attributes: {
+		id: {
+			type: 'int'
+		},
+
+		username: {
+			type: 'string'
+		},
+
+		email: {
+			type: 'string'
+		},
+
+		password: {
+			type: 'string'
+		},
+
+		fullname: {
+			type: 'string'
+		}
+	}
+}
