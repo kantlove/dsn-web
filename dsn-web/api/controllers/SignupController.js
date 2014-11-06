abe914d65e35a75b44fe0a466f9cb4a90c7948ce
nminhtu94 <nminhtu94@gmail.com>
2014-11-06 11:45:22 +0700

    Signup Controller with post request

--- dsn-web/api/controllers/SignupController.js
+++ dsn-web/api/controllers/SignupController.js
@@ -1,5 +1,37 @@
+var networkHelper = require("../models/NetworkHelper.js");
+var http = require('http');
+
 module.exports = {
-  processSignup: function (req, res) {
-    return res.send(req.body.username);
-  }
+    processSignup: function (req, res) {
+    	console.log("Make signup request");
+    	var options = {
+		  host: 'http://dreamyday.tk/docs/#!/user/createUser_post_1',
+		  path: '/',
+		  headers: {'username': req.body.username,
+		  			'fullname': req.body.fullname,
+		  			'email':    req.body.email,
+		  			'password': req.body.password
+					},
+
+		  method: 'POST'
+		};
+
+		callback = function(response) {
+		  var str = '';
+
+		  //another chunk of data has been recieved, so append it to `str`
+		  response.on('data', function (chunk) {
+		    str += chunk;
+		  });
+
+		  //the whole response has been recieved, so we just print it out here
+		  response.on('end', function () {
+		    console.log(str);
+		    return res.send(str);
+		  });
+		}
+
+	  	http.request(options, callback).end();
+	    //return res.send(req.body.username);
+	 }
 };
